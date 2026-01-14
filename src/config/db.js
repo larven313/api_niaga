6cea3f2710caca0aecb9a6354e371fc3186c70fd
Sukma Hidayatullah <hidayatullahsukma@gmail.com>
2026-01-14 16:57:34 +0700

    refactor db.js hapus authenticate, itu hanya ada di index.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,22 +5,12 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: "mysql",
     logging: process.env.NODE_ENV === "development" ? console.log : false,
     timezone: "+07:00",
   }
 );
-
-// Test koneksi (aman)
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ Koneksi database berhasil");
-  } catch (error) {
-    console.error("❌ Koneksi database gagal:", error.message);
-  }
-})();
 
 module.exports = sequelize;
